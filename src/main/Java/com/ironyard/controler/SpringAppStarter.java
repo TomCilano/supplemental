8df22744635b66e26fa340c867417841ebebfa2c
Thomas Cilano <Tom@Thomass-MacBook-Pro.local>
2016-10-10 14:58:33 -0400

    firstCommit

---
+++ src/main/Java/com/ironyard/controler/SpringAppStarter.java
@@ -0,0 +1,12 @@
+package com.ironyard.controler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppStarter.class, args);
+    }
+}
